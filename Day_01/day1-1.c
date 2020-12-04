d66b7c817e79a3ba21f12aae4cb2ab72265c99c2
Axilot <sebastian.kirscha@gmx.de>
2020-12-04 20:17:19 +0100

    Adding Day_01

---
+++ Day_01/day1-1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+#define MAX_NUMBERS 1000 /* Sets a limit on how many numbers can actually be saved */
+#define MAX_DIGITS 8 /* Sets a limit on how many digits can be stored in each number */
+
+/* Save all numbers into array */
+void saveNumbersFromFile(int numbers[]);
+
+int main() {
+    int numbers[MAX_NUMBERS];
+    int i, n;
+    for (i = 0; i < MAX_NUMBERS; i++) {
+        numbers[i] = 0;
+    }
+
+    saveNumbersFromFile(numbers);
+
+    for (i = 0; i < MAX_NUMBERS; i++) {
+        for (n = 0; n < MAX_NUMBERS; n++) {
+            if (numbers[i] + numbers[n] == 2020 && numbers[i] != 0 && numbers[n] != 0) {
+                printf("The searched number is: %d\n", numbers[i] * numbers[n]);
+                goto finished;
+            }
+        }
+    }
+
+    finished:
+    return 0;
+}
+
+void saveNumbersFromFile(int numbers[]) {
+    int currNumber[MAX_DIGITS]; /* Stores the current number in special format */
+    int i, n, z, c;
+    int digit; /* Stores the multiplier for my number saving technique and the curren digit */
+    
+    /* Set some initial values */
+    i = z = n = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            int num, mult;
+
+            num = 0;
+            mult = 1;
+
+
+            for (z = i-1; z >= 0; z--) {
+                num += currNumber[z] * mult;
+                mult *= 10;
+            }
+
+            numbers[n] = num;
+            n++;
+
+            for (i = MAX_DIGITS-1; i >= 0; i--) {
+                currNumber[i] = 0;
+            }
+
+            i = 0;
+        }
+        else {
+            if (c >= '0' && c <= '9') {
+                digit = c - '0';
+                currNumber[i] = digit;
+                i++;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+# AdventOfCode C Solutions
+
+
+Hello Adventurer, this year's winter is very cold. But there is hope: Advent of Code 2020 is there. This repo is a work-in-progress to fully cover all challenges. So have fun with it :D
+
+
+### Current Status
+
+- [x] Day 1
+- [ ] Day 2
+- [ ] Day 3
+- [ ] Day 4
+
+## How do i compile the solutions?
+
+You simply clone the repository, head into the solution you want to see and run `rake`. After running `rake` you should have now have the executables. I included my inputs as a possibility to test the code. And please keep in mind, that this is my first github project, so please be kind with me :D
+
+
+### Example:
+```
+git clone https://github.com/Axilot/adventofcode.git
+cd Day_01
+rake release
+./build/day1-1 < day1_input
+```
+
+## Dependencies
+
+### Arch
+
+```
+sudo pacman -S clang ruby-rake
+```
+### Debian / Ubuntu
+```
+sudo apt-get install rake clang
+```
+
+
+## Contribute!
+
+I would really enjoy if you take your time and search for improvements. I really like learning new things and improving my code. So feel free to create issues and pull requests. And if you change code explain why you changed that code and what makes your change "better". And there's just one more thing: Please do not include any third party libraries. Thanks!
